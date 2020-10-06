aa8779a1ccb21246a9d099a626eb800aa2279db9
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-06 16:16:14 +0900

    add : template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+#define ll long long
+#define rep(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define Graph vector<vector<int>>;
+#define iterG(next_v, G, v) for(auto next_v : G[v]
+const int MOD = 1000000007;
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+}
