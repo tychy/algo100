566980ff3e69d24d326d540f0994b9e7f22e514b
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-17 17:30:39 +0900

    add : update 7

--- 1to10/7.cpp
+++ 1to10/7.cpp
@@ -10,60 +10,36 @@ using namespace std;
 #define Graph vector<vector<int>>;
 #define iterG(next_v, G, v) for(auto next_v : G[v]
 const int MOD = 1000000007;
-bool isvalid(pair<int, int> a)
+void printer(vector<int> v)
 {
-    return a.first >= 0 and a.second >= 0;
-}
-pair<int, int> minus_pair(pair<int, int> a, pair<int, int> b)
-{
-    return make_pair(a.first - b.first, a.second - b.second);
-}
-pair<int, int> plus_pair(pair<int, int> a, pair<int, int> b)
-{
-    return make_pair(a.first + b.first, a.second + b.second);
-}
-pair<int, int> t_pair(pair<int, int> a)
-{
-    return make_pair(a.second, -a.first);
-}
-pair<int, int> inv_pair(pair<int, int> a)
-{
-    return make_pair(-a.first, -a.second);
-}
-bool isexist(pair<int, int> a, map<pair<int, int>, int> mp)
-{
-    if (not isvalid(a))
+    rep(i, v.size())
     {
-        return false;
+        cout << v[i] << " ,";
     }
-    auto itr = mp.find(a);
-    return itr != mp.end();
+    cout << endl;
 }
-int issquare(pair<int, int> a, pair<int, int> b, map<pair<int, int>, int> v)
+bool find(pair<int, int> p, const vector<int> &x, const vector<int> &y)
 {
-    pair<int, int> diff = minus_pair(a, b);
-    pair<int, int> p_ver = t_pair(diff);
-    pair<int, int> m_ver = inv_pair(p_ver);
-    int area = diff.first * diff.first + diff.second * diff.second;
-    if (isexist(plus_pair(a, p_ver), v) and isexist(plus_pair(b, p_ver), v))
+    auto iterl = lower_bound(x.begin(), x.end(), p.first);
+    int idx = distance(x.begin(), iterl);
+    for (int i = idx; i < x.size(); i++)
     {
-        return area;
+        if (x[i] != p.first)
+        {
+            return false;
+        }
+        if (y[i] == p.second)
+        {
+            return true;
+        }
     }
-    else if (isexist(plus_pair(a, m_ver), v) and isexist(plus_pair(b, m_ver), v))
-    {
-        return area;
-    }
-    else
-    {
-        return 0;
-    }
+    return false;
 }
 int main()
 {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
     int n, inputx, inputy;
-    map<pair<int, int>, int> mp;
     vector<pair<int, int>> vp;
     int score = 0;
     int area;
@@ -71,19 +47,35 @@ int main()
     rep(i, n)
     {
         cin >> inputx >> inputy;
-        mp[make_pair(inputx, inputy)] = 1;
         vp.push_back(make_pair(inputx, inputy));
     }
-    rep(i, n - 1)
+    //sort vp by x
+    sort(vp.begin(), vp.end());
+    vector<int> x(n), y(n);
+    rep(i, n)
+    {
+        x[i] = vp[i].first;
+        y[i] = vp[i].second;
+    }
+    pair<int, int> pa, pb, pc, pd;
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
-            area = issquare(vp[i], vp[i + 1], mp);
-            if (area > score)
+            pa = vp[i];
+            pb = vp[j];
+            pc = make_pair(pa.first + pa.second - pb.second, pa.second + pb.first - pa.first);
+            pd = make_pair(pb.first + pa.second - pb.second, pb.second + pb.first - pa.first);
+            area = (pb.first - pa.first) * (pb.first - pa.first) + (pb.second - pa.second) * (pb.second - pa.second);
+            if (find(pc, x, y) and find(pd, x, y))
             {
-                score = area;
+                if (area > ans)
+                {
+                    ans = area;
+                }
             }
         }
     }
-    cout << score << endl;
+    cout << ans << endl;
 }
