835963ad73858326b10af456fbe6e270904e3607
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-10 17:00:20 +0900

    add : 10

--- 2nd/1to10/10.cpp
+++ 2nd/1to10/10.cpp
@@ -12,7 +12,50 @@ using namespace std;
 int main()
 {
     int n, q;
-    vector<int> a(n), m(q);
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cin >> q;
+    vector<int> m(q);
+    vector<bool> ans(q, false);
+
+    for (int i = 0; i < q; i++)
+    {
+        cin >> m[i];
+    }
+    bool flag = false;
+    for (int bit = 0; bit < (1 << n); bit++)
+    {
+        int sums = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (bit & (1 << i))
+            {
+                sums += a[i];
+            }
+        }
+        for (int i = 0; i < q; i++)
+        {
+            if (m[i] == sums)
+            {
+                ans[i] = true;
+            }
+        }
+    }
+    for (int i = 0; i < q; i++)
+    {
+        if (ans[i])
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
 
     return 0;
 }
